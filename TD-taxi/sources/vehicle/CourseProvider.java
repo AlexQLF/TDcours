c91f61dcfa7c0a70847cc7798e0a3bb81541cf9c
AlexQLF <64869991+AlexQLF@users.noreply.github.com>
2020-11-04 10:18:39 +0100

    Add files via upload

---
+++ TD-taxi/sources/vehicle/CourseProvider.java
@@ -0,0 +1,25 @@
+package vehicle;
+
+public class CourseProvider extends Thread {
+	
+	private final int idProvider;
+	private final int nbcourse;
+
+	private final Plateforme p;
+	
+	CourseProvider(int id, int nb, Plateforme p){
+		this.idProvider = id;
+		this.nbcourse = Math.abs(nb);
+		this.p = p;
+	}
+	
+	public void run(){
+		for(int i=0;i<nbcourse;++i){
+			try{wait();}catch (InterruptedException e) {}
+			p.addCourse(new Course("course_"+i, (i+1)*3 ));
+		}
+		System.out.println("Fin du thread t" + this.idProvider);
+	}
+	
+	
+}
